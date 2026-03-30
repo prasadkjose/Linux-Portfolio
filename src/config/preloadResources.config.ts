4677a12cdf9a874aad331bbb323188aed0dc56b6
prasad koshy jose <prasadkjose@gmail.com>
2026-03-30 10:10:30 -0600

    Add: icons

--- src/config/preloadResources.config.ts
+++ src/config/preloadResources.config.ts
@@ -1,1 +1,12 @@
+import theme from "../styles/themes";
+import { DefaultTheme } from "styled-components";
+
 export const IMAGES = ["photo1.jpg"];
+
+export const CDN_IMAGES: string[] = [];
+
+CDN_IMAGES.push(
+  ...Object.values(theme)
+    .map((t: DefaultTheme) => t?.backgroundImage)
+    .filter((bg): bg is string => Boolean(bg))
+);
