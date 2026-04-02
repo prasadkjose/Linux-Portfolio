bce77821bcd9caa55863791be4a2f7b60f034c45
prasad koshy jose <prasadkjose@gmail.com>
2026-04-01 19:18:16 -0600

    Fix: Static Resource importing for theme specific resource

--- src/config/preloadResources.config.ts
+++ src/config/preloadResources.config.ts
@@ -1,12 +1,3 @@
-import theme from "../styles/themes";
-import { DefaultTheme } from "styled-components";
-
 export const IMAGES = ["photo1.jpg", "prasadkjose-full.svg", "favicon.svg"];
 
 export const CDN_IMAGES: string[] = [];
-
-CDN_IMAGES.push(
-  ...Object.values(theme)
-    .map((t: DefaultTheme) => t?.backgroundImage)
-    .filter((bg): bg is string => Boolean(bg))
-);
