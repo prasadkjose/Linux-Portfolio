7bcf73e769c641ee6b7797c7a5bc1f79b1e7ef18
prasad koshy jose <prasadkjose@gmail.com>
2026-04-06 09:36:36 -0600

    Add: Tooltip config framework

---
+++ src/layout/tooltips/tooltips.config.ts
@@ -0,0 +1,23 @@
+import { isMobileDevice } from "../../utils/typeGuards";
+import type { TooltipProps } from "./Tooltip";
+
+export interface TooltipConfigItem extends Pick<TooltipProps, "id"> {
+  content: string;
+  position: "bottom-left" | "bottom-right" | "top-left" | "top-right";
+  showAfter?: number;
+}
+
+export const TOOLTIPS_CONFIG: TooltipConfigItem[] = [
+  {
+    id: "fullscreen-hint",
+    showAfter: 1000,
+    position: "top-right",
+    content: "💡 For best experience tap here to use fullscreen mode",
+  },
+  {
+    id: "theme-switcher-hint",
+    showAfter: 1000,
+    position: `${isMobileDevice() ? "top-left" : "bottom-right"}`,
+    content: "⚙️ Try out a different Linux here.",
+  },
+];
