05660f2343ae1c97c033cf8a4f412087c389aabd
prasad koshy jose <prasadkjose@gmail.com>
2026-04-07 14:31:14 -0600

    Fix: tab support at bowser level

--- src/layout/browser/BrowserRouter.tsx
+++ src/layout/browser/BrowserRouter.tsx
@@ -1,31 +1,61 @@
 import { WindowState } from "../../types/window";
 import BrowserWindow from "../window-container/BrowserWindow";
 import BROWSER_ROUTER_CONFIG from "./browserRouter.config";
+import Tabs, { TabData } from "../tabs/Tabs";
+import { useEffect, useState } from "react";
 
 const DEFAULT_ROUTE: string = "/home";
 
 const BrowserRouter: React.FC<WindowState> = props => {
   const { href = DEFAULT_ROUTE } = props;
-
+  const [tabs, setTabs] = useState<TabData[]>([]);
+  // If route exists in config
   if (href && href in BROWSER_ROUTER_CONFIG) {
     const routeConfig =
       BROWSER_ROUTER_CONFIG[href as keyof typeof BROWSER_ROUTER_CONFIG];
     const Component = routeConfig.component;
+    // Always initialize with home tab if empty
+    if (tabs.length === 0) {
+      const homeConfig = BROWSER_ROUTER_CONFIG["/home"];
+      const homeTab: TabData = {
+        id: href,
+        label: href.charAt(1).toUpperCase() + href.slice(2) || href,
+        content: () => (
+          <Tabs {...props} tabs={homeConfig.tabs.welcome} activeTabId="home" />
+        ),
+      };
+      setTabs(prev => [...prev, homeTab]);
+    }
+
+    useEffect(() => {
+      // Create new tab from path
+      const newTab: TabData = {
+        id: href,
+        label: href,
+        content: () => <Component tabs={[]} {...props} />,
+      };
+
+      // Only add if tab doesn't already exist
+      if (!tabs.find(tab => tab.id === href)) {
+        setTabs(prev => [...prev, newTab]);
+      }
+    }, [href]);
 
     return (
       <BrowserWindow {...{ ...props, href }}>
-        {"tabs" in routeConfig ? (
-          <Component
-            {...props}
-            tabs={routeConfig.tabs.welcome}
-            activeTabId="home"
-          />
-        ) : (
-          <Component tabs={[]} {...props} />
-        )}
+        <Tabs
+          tabs={tabs}
+          activeTabId={href}
+          allowClose={true}
+          onCloseTab={tabId =>
+            setTabs(prev => prev.filter(tab => tab.id !== tabId))
+          }
+          {...props}
+        />
       </BrowserWindow>
     );
   }
+
   return null;
 };
 
